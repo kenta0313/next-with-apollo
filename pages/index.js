cd3d6926d32da7dc42d2a45dbc4a12c90e5a4398
kenta0313 <asanokenta@me.com>
2021-07-05 18:33:22 +0900

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
-export default function Home() {
+export default function Home({ countries }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -66,4 +68,24 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+ };
 }
